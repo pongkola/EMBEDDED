4870ad109d9a6d50ab757f615e06aba8c56e13c7
pongkola <56030276@kmitl.ac.th>
2017-05-02 00:27:17 +0700

    Add files via upload

---
+++ SOS_LAB_1/SOS_LAB_1.ino
@@ -0,0 +1,48 @@
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(7, OUTPUT);
+}
+void loop() {
+  digitalWrite(7, HIGH); 
+  delay(100);                    
+  digitalWrite(7, LOW);   
+  delay(100);       
+  digitalWrite(7, HIGH); 
+  delay(100);                    
+  digitalWrite(7, LOW);   
+  delay(100);     
+  digitalWrite(7, HIGH); 
+  delay(100);                    
+  digitalWrite(7, LOW);   
+  delay(100);
+
+
+  digitalWrite(7, HIGH); 
+  delay(1000);                    
+  digitalWrite(7, LOW);   
+  delay(1000);       
+  digitalWrite(7, HIGH); 
+  delay(1000);                    
+  digitalWrite(7, LOW);   
+  delay(1000);     
+  digitalWrite(7, HIGH); 
+  delay(1000);                    
+  digitalWrite(7, LOW);   
+  delay(1000);
+
+
+
+  digitalWrite(7, HIGH); 
+  delay(100);                    
+  digitalWrite(7, LOW);   
+  delay(100);       
+  digitalWrite(7, HIGH); 
+  delay(100);                    
+  digitalWrite(7, LOW);   
+  delay(100);     
+  digitalWrite(7, HIGH); 
+  delay(100);                    
+  digitalWrite(7, LOW);   
+  delay(1000);
+}
