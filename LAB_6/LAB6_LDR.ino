4f2d2298146a570f9a0ab5ff9b79cb95e742e289
pongkola <56030276@kmitl.ac.th>
2017-05-05 14:17:22 +0700

    Add files via upload

---
+++ LAB_6/LAB6_LDR.ino
@@ -0,0 +1,14 @@
+int sensorPin = A0;
+int sensorValue = 0;
+void setup() {
+ pinMode(12, OUTPUT);
+ Serial.begin(9600);
+}
+void loop() {
+ sensorValue = analogRead(sensorPin);
+ Serial.println(sensorValue);
+ if(sensorValue < 500)
+digitalWrite(12,HIGH);
+else digitalWrite(12,LOW);
+delay(100);
+}
